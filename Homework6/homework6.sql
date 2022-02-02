231c3e1ad8272dd7a6b032ad288fa44b63441a26
Sinan42 <81928662+Sinan42@users.noreply.github.com>
2022-02-02 17:10:40 +0300

    Add files via upload

---
+++ Homework6/homework6.sql
@@ -0,0 +1,14 @@
+SELECT ROUND(AVG(rental_rate),3) FROM film;
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+
+SELECT MAX(length) from film
+WHERE rental_rate=0.99;
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length>150;
+
+
+
